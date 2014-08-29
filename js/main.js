40d0d2069ea4519620cc4811d383ea84de09d211
Mikolaj Adamczyk <mikolaj.adamczyk@vml.com>
2014-08-29 11:31:29 +0200

    Using Handlebar Templates

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
 
     initialize: function() {
         var self = this;
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new MemoryStore(function(){
-            self.showAlert('Store Initioalized', 'Info');
+            self.renderHomeView();
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
@@ -27,8 +23,12 @@ var app = {
         } else {
             alert(title ? (title + ": " + message) : message);
         }
-    }
+    },
 
+    renderHomeView: function(){
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    }
 };
 
 app.initialize();
